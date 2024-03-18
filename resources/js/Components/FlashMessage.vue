c38625162a7c812ece5a3a23219f9ca587a97e6a
Noriaki Fujimaru <houken@me.com>
2024-03-18 22:01:52 +0900

    Item delete

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -5,6 +5,12 @@
     >
         {{ $page.props.flash.message }}
     </div>
+    <div
+        v-if="$page.props.flash.status === 'danger'"
+        class="bg-red-300 text-white my-4 p-4"
+    >
+        {{ $page.props.flash.message }}
+    </div>
 </template>
 
 <script setup>
